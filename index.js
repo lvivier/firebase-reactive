0ade5fabab259018f6262d2d9cba7a2d6b0a3f5f
Luke Vivier <luke@vivier.ca>
2014-01-16 23:31:32 -0800

    emit error events

--- index.js
+++ index.js
@@ -23,10 +23,13 @@ function React (db) {
   db.on('child_removed', change)
   db.on('child_changed', change)
   db.on('child_added', change)
-  db.once('value', ready)
+  db.once('value', ready, error)
 
   // ready handler
   function ready () { me.emit('ready') }
+
+  // error handler
+  function error (err) { me.emit('error', err) }
 
   // change handler
   function change (child) {
